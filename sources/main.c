2e06e9c6dec1acd1c3be73d2a1a8bfc44f728eba
Velocitypulse <velocitypulse@outlook.fr>
2016-09-18 13:58:48 +0200

    update of security in main

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: jlasne <jlasne@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/15 10:15:56 by jlasne            #+#    #+#             */
-/*   Updated: 2016/09/18 13:56:24 by                  ###   ########.fr       */
+/*   Updated: 2016/09/18 13:58:18 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,10 @@ int		main(int argc, char **argv)
 {
 	t_word_gen		var;
 
-	if (argc < 2)
+	if (argc != 3)
 		return (0);
 	var.w_nb = ft_atoi(argv[1]);
 	var.w_size = ft_atoi(argv[2]);
-	if (argc != 3)
-		return (-1);
 	print_error(generator(&var));
 	return (0);
 }
